bc2ee863515c46e0cdec65239a12e38ecce6bb47
Dmitry Danov <dmitry.danau@juriba.com>
2025-08-03 14:12:53 +0300

    Implement activation service

---
+++ ASRE/ASRE.PatientApi/Services/Activation/ActivationService.cs
@@ -0,0 +1,46 @@
+﻿using ASRE.DataLayer.Context;
+using ASRE.PatientApi.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASRE.PatientApi.Services.Activation;
+
+public sealed class ActivationService : IActivationService
+{
+    private readonly AppDbContext _dbContext;
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ActivationService"/> class.
+    /// </summary>
+    /// <param name="dbContext">An instance of the <see cref="AppDbContext"/> class.</param>
+    /// <param name="logger">An instance of a class which implements <see cref="ILogger{ActivationService}"/>.</param>
+    public ActivationService(AppDbContext dbContext, ILogger<ActivationService> logger)
+    {
+        ArgumentNullException.ThrowIfNull(dbContext);
+
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    /// <inheritdoc/>
+    public async Task<IOperationDataResult<DataLayer.Models.Activation>> GetActivationAsync(string? name, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return OperationDataResult<DataLayer.Models.Activation>.Success(await _dbContext.Activations.FirstOrDefaultAsync(w => w.Name == name, cancellationToken).ConfigureAwait(false));
+        }
+        catch (Exception exception)
+        {
+            if (exception is ArgumentNullException)
+            {
+                _logger.LogError(exception, "An exception has happened during retrieving '{Activation}' activation.", name);
+
+                return OperationDataResult<DataLayer.Models.Activation>.Failure(exception);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ASRE/ASRE.PatientApi/Services/Activation/IActivationService.cs
@@ -0,0 +1,14 @@
+﻿using ASRE.PatientApi.Core;
+
+namespace ASRE.PatientApi.Services.Activation;
+
+public interface IActivationService
+{
+    /// <summary>
+    /// Gets activation by its name.
+    /// </summary>
+    /// <param name="name">An activation name.</param>
+    /// <param name="cancellationToken">A cancellation token which is used to cancel get operation.</param>
+    /// <returns>The task that represents an asynchronous get operation which result is <see cref="IOperationDataResult{DataLayer.Models.Activation}"/>.</returns>
+    Task<IOperationDataResult<DataLayer.Models.Activation>> GetActivationAsync(string? name, CancellationToken cancellationToken = default);
+}

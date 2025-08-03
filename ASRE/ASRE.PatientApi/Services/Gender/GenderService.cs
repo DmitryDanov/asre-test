16db284a4cf3c3e185cb31a8ae8fa8cf4285f520
Dmitry Danov <dmitry.danau@juriba.com>
2025-08-03 14:12:35 +0300

    Implement gender service

---
+++ ASRE/ASRE.PatientApi/Services/Gender/GenderService.cs
@@ -0,0 +1,47 @@
+﻿using ASRE.DataLayer.Context;
+using ASRE.PatientApi.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASRE.PatientApi.Services.Gender;
+
+public sealed class GenderService : IGenderService
+{
+    private readonly AppDbContext _dbContext;
+    private readonly ILogger _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenderService"/> class.
+    /// </summary>
+    /// <param name="dbContext">An instance of the <see cref="AppDbContext"/> class.</param>
+    /// <param name="logger">An instance of a class which implements <see cref="ILogger{GenderService}"/>.</param>
+    public GenderService(AppDbContext dbContext, ILogger<GenderService> logger)
+    {
+        ArgumentNullException.ThrowIfNull(dbContext);
+        ArgumentNullException.ThrowIfNull(logger);
+
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    /// <inheritdoc/>
+    public async Task<IOperationDataResult<DataLayer.Models.Gender>> GetGenderAsync(string? name, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return OperationDataResult<DataLayer.Models.Gender>.Success(await _dbContext.Genders.FirstOrDefaultAsync(w => w.Name == name, cancellationToken).ConfigureAwait(false));
+        }
+        catch (Exception exception)
+        {
+            if (exception is ArgumentNullException)
+            {
+                _logger.LogError(exception, "An exception has happened during retrieving '{Gender}' gender.", name);
+
+                return OperationDataResult<DataLayer.Models.Gender>.Failure(exception);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ASRE/ASRE.PatientApi/Services/Gender/IGenderService.cs
@@ -0,0 +1,15 @@
+﻿using ASRE.PatientApi.Core;
+
+namespace ASRE.PatientApi.Services.Gender
+{
+    public interface IGenderService
+    {
+        /// <summary>
+        /// Gets gender by its unique identifier.
+        /// </summary>
+        /// <param name="name">A gender name.</param>
+        /// <param name="cancellationToken">A cancellation token which is used to cancel get operation.</param>
+        /// <returns>The tas that represents an asynchronous get operation which result is <see cref="IOperationDataResult{DataLayer.Models.Gender}"/>.</returns>
+        Task<IOperationDataResult<DataLayer.Models.Gender>> GetGenderAsync(string? name, CancellationToken cancellationToken = default);
+    }
+}

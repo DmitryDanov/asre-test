91f6e01de1ff111e9d0219629682541bce19aee7
Dmitry Danov <dmitry.danau@juriba.com>
2025-08-04 17:32:57 +0300

    Add initial EF migration

---
+++ ASRE/ASRE.PatientApi/Migrations/20250731172645_Initial.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ASRE.PatientApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FamilyName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Use = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true),
+                    GenderId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ActivationId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Patients_Activations_ActivationId",
+                        column: x => x.ActivationId,
+                        principalTable: "Activations",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Patients_Genders_GenderId",
+                        column: x => x.GenderId,
+                        principalTable: "Genders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PatientGivenNames",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PatientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatientGivenNames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatientGivenNames_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Activations",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("350fda9b-d8e0-4df4-ad3f-9a374c9e0801"), "true" },
+                    { new Guid("920b297b-8827-45ca-9d9b-f3eea9f2fcb4"), "false" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genders",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("38f4e1cc-440c-4e10-94c9-af1db3364f07"), "unknown" },
+                    { new Guid("7c610b7c-9a23-4f54-bf1f-e9d5d02b6b5d"), "male" },
+                    { new Guid("e97c5b5d-484c-43e9-8850-d65c57579875"), "other" },
+                    { new Guid("ecb9f689-9427-4e27-8c23-2ac2c3e3d0a5"), "female" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatientGivenNames_PatientId",
+                table: "PatientGivenNames",
+                column: "PatientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_ActivationId",
+                table: "Patients",
+                column: "ActivationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_GenderId",
+                table: "Patients",
+                column: "GenderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PatientGivenNames");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "Activations");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+        }
+    }
+}
